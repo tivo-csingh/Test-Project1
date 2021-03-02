a5856265327aae4518d84c42be43d014c5d7e82e
tivo-csingh <57895493+tivo-csingh@users.noreply.github.com>
2021-03-02 12:18:30 +0530

    Adding divison function

--- math_operations.py
+++ math_operations.py
@@ -7,3 +7,9 @@ def diff(num1,num2):
         return num1 - num2
     else:
         return False
+
+def multiply(num1,num2):
+    return num1 * num2
+
+def division(num1,num2):
+    return num1/num2
